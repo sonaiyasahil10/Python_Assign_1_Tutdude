17fb7f2dbebce9abe648f1e930d4679da19f5d80
sonaiyasahil10 <sonaiyasahil10@gmail.com>
2025-06-06 18:22:30 +0530

    Updated Solution

---
+++ Task1.py
@@ -0,0 +1,9 @@
+#Task 1: Perform Basic Mathematical Operations
+
+a= int(input("Enter the first number: "))
+b= int(input("Enter the second number: "))
+
+print("Addition: ", a+b)
+print("Subtraction: ", a-b)
+print("Multiplication: ", a*b)
+print("Division: ", a/b)
---
+++ Task2.py
@@ -0,0 +1,6 @@
+#Task 2: Create a Personalized Greeting
+
+first_name = input("Enter you first name: ")
+last_name = input("Enter you last name: ")
+
+print(f"Hello, {first_name+" "+last_name}! Welcome to the python program.")
